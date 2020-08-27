bbc362dca8f99f20e5c256a595aa5c687191b32e
Ondřej Němec <nikdynikde@gmail.com>
2020-08-27 16:23:43 +0200

    response factory added dir response

--- src/main/java/mvc/ResponseFactory.java
+++ src/main/java/mvc/ResponseFactory.java
@@ -1,20 +1,35 @@
 package mvc;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
 
+import mvc.response.Response;
+import mvc.templating.DirectoryTemplate;
 import mvc.templating.ExceptionTemplate;
-import mvc.templating.Template;
 import socketCommunication.http.HttpMethod;
 import socketCommunication.http.StatusCode;
 import socketCommunication.http.server.RestApiResponse;
 import socketCommunication.http.server.RestApiServerResponseFactory;
+import socketCommunication.http.server.session.Session;
 
 public class ResponseFactory implements RestApiServerResponseFactory {
 	
 	private final List<String> headers = new LinkedList<>();
+	
+	private final String resourcesDir;
+	private final String charset;
+	
+	public ResponseFactory(String resourcesDir, String charset) {
+		this.resourcesDir = resourcesDir;
+		this.charset = charset;
+	}
+	
+	public void addHeader(String header) {
+		headers.add(header);
+	}
 
 	@Override
 	public RestApiResponse accept(
@@ -23,19 +38,40 @@ public class ResponseFactory implements RestApiServerResponseFactory {
 			String fullUrl,
 			String protocol,
 			Properties header,
-			Properties params) throws IOException {
-		try {
-			
-		} catch (Throwable t) {
-			return RestApiResponse.textResponse(StatusCode.OK, headers, (bw)->{
-				try {
-					bw.write(new ExceptionTemplate(t).create(null, null, null));
-				} catch (Exception e) {
-					throw new RuntimeException(e);
-				}
-			});
+			Properties params,
+			Session session) throws IOException {
+		// TODO mapping
+		File file = new File(resourcesDir + url);
+		if (file.isDirectory()) {
+			return getDirResponse(file.listFiles(), url);
 		}
-		return null;
+		return Response.getFile(resourcesDir + url, charset).getResponse(headers, null, null);
+	}
+	
+	private RestApiResponse getDirResponse(File[] files, String path) {
+		List<String> h = new LinkedList<>(headers);
+		h.add("Content-Type: text/html; charset=" + charset);
+		return RestApiResponse.textResponse(StatusCode.OK, h, (bw)->{
+			try {
+				bw.write(new DirectoryTemplate(files, path).create(null, null, null));
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		});
+	}
+	
+	@Override
+	public RestApiResponse onException(HttpMethod method, String url, String fullUrl, String protocol,
+			Properties header, Properties params, Session session, Throwable t) throws IOException {
+		List<String> h = new LinkedList<>(headers);
+		h.add("Content-Type: text/html; charset=" + charset);
+		return RestApiResponse.textResponse(StatusCode.OK, h, (bw)->{
+			try {
+				bw.write(new ExceptionTemplate(t).create(null, null, null));
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		});
 	}
 
 }
