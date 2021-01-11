fa358bbd87308350ff95ac3ea6c9c01842d3ab1d
Ondřej Němec <nikdynikde@gmail.com>
2021-01-11 22:27:34 +0100

    code style

--- src/main/java/toti/templating/parsing/TemplateParser.java
+++ src/main/java/toti/templating/parsing/TemplateParser.java
@@ -3,7 +3,6 @@ package toti.templating.parsing;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
