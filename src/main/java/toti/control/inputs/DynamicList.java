4f0a513b36523dfbba72bc42703718dff2e32026
Ondřej Němec <nikdynikde@gmail.com>
2021-03-06 00:26:33 +0100

    added input list and dynamic list

---
+++ src/main/java/toti/control/inputs/DynamicList.java
@@ -0,0 +1,74 @@
+package toti.control.inputs;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class DynamicList implements Input {
+
+	private final String type = "dynamic";
+	private final String name;
+	private final String id;
+	private final List<Input> fields;
+	
+	private String title = null;;
+	private boolean useAddButton = true;
+	private boolean useRemoveButton = true;
+	
+	public static DynamicList input(String name) {
+		return new DynamicList(name);
+	}
+	
+	private DynamicList(String name) {
+		this.name = name;
+		this.fields = new LinkedList<>();
+		this.id = "id-" + name;
+	}
+	
+	public DynamicList addInput(Input input) {
+		fields.add(input);
+		return this;
+	}
+	
+	public DynamicList setTitle(String title) {
+		this.title = title;
+		return this;
+	}
+	
+	/**
+	 * Has evect only without form template
+	 * @param useAddButton
+	 * @return
+	 */
+	public DynamicList useAddButton(boolean useAddButton) {
+		this.useAddButton = useAddButton;
+		return this;
+	}
+	
+	/**
+	 * Has evect only without form template
+	 * @param useRemoveButton
+	 * @return
+	 */
+	public DynamicList useRemoveButton(boolean useRemoveButton) {
+		this.useRemoveButton = useRemoveButton;
+		return this;
+	}
+	
+	@Override
+	public Map<String, Object> getInputSettings() {
+		Map<String, Object> json = new HashMap<>();
+		json.put("name", name);
+		json.put("id", id);
+		json.put("type", type);
+		json.put("fields", fields);
+		if (title != null) {
+			json.put("title", title);
+		}
+		json.put("addButton", useAddButton);
+		json.put("removeButton", useRemoveButton);
+		return json;
+	}
+
+}
---
+++ src/main/java/toti/control/inputs/InputList.java
@@ -0,0 +1,47 @@
+package toti.control.inputs;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class InputList implements Input {
+
+	private final String type;
+	private final String name;
+	private final String id;
+	private final List<Input> fields;
+	
+	public static InputList input(String name) {
+		return new InputList(name);
+	}
+	
+	public static InputList input() {
+		return new InputList(null);
+	}
+	
+	private InputList(String name) {
+		this.name = name;
+		this.id = "id-" + name;
+		this.type = "list";
+		this.fields = new LinkedList<>();
+	}
+	
+	public InputList addInput(Input input) {
+		fields.add(input);
+		return this;
+	}
+	
+	@Override
+	public Map<String, Object> getInputSettings() {
+		Map<String, Object> json = new HashMap<>();
+		if (name != null) {
+			json.put("name", name);
+		}
+		json.put("id", id);
+		json.put("type", type);
+		json.put("fields", fields);
+		return json;
+	}
+	
+}
