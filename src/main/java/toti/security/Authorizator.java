d0825d871eec59e7409760bf7bd06ceac68a9a89
Ondřej Němec <nikdynikde@gmail.com>
2021-04-14 22:16:11 +0200

    wip authorizator

--- src/main/java/toti/security/Authorizator.java
+++ src/main/java/toti/security/Authorizator.java
@@ -2,8 +2,20 @@ package toti.security;
 
 public class Authorizator {
 
+	public boolean isAllowed(User user, String domain, Action action) {
+		try {
+			authorize(user, domain, action);
+			return true;
+		} catch(AccessDeniedException e) {
+			return false;
+		}
+	}
+	
 	public void authorize(User user, String domain, Action action) {
 		// TODO
+		/*
+		 * get where user id a domain
+		 */
 	}
 
 	public void authorize(User user, String domain, Action action, Object ownerId) {
