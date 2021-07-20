333e1bc0a2c7ed502231b3e8782c00108f68fe53
Ondřej Němec <ondrej.nemec@autmes.cz>
2021-07-20 11:19:46 +0200

    entity two ways how serialize

--- src/main/java/toti/application/Entity.java
+++ src/main/java/toti/application/Entity.java
@@ -3,12 +3,17 @@ package toti.application;
 import java.util.Map;
 
 import common.functions.Mapper;
+import json.Jsonable;
 
-public interface Entity {
+public interface Entity extends Jsonable {
 
 	default Map<String, Object> toMap() {
-		return Mapper.get().serialize(this);
+		return Mapper.get().serialize(this, "database");
 	}
 	
+	@Override
+	default Object toJson() {
+		return Mapper.get().serialize(this, "json");
+	}
 	
 }
