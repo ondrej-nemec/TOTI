186c66cae041eec359d2f472164d7f5ccc3aa96a
Ondřej Němec <nikdynikde@gmail.com>
2022-01-07 23:25:29 +0100

    wip doc

--- src/test/java/samples/getstarted/init/CustomInit.java
+++ src/test/java/samples/getstarted/init/CustomInit.java
@@ -4,32 +4,39 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-import ji.common.Logger;
 import ji.common.functions.Env;
 import ji.database.Database;
 import toti.HttpServer;
 import toti.HttpServerFactory;
 import toti.Module;
-import toti.application.Task;
-import toti.registr.Register;
 
 public class CustomInit {
 
 	public static void main(String[] args) {
-		Database database = null; // can be null
-		Env env = null; // can be null
 		List<Module> modules = Arrays.asList(
 			// modules
 		);
-		List<Task> tasks = new LinkedList<>();
+		// if you wish use migrations:
+		/*
+		List<String> migrations = new LinkedList<>();
+		modules.forEach((config)->{
+			if (config.getMigrationsPath() != null) {
+				migrations.add(config.getMigrationsPath());
+			}
+		});
+		*/
+		Database database = null; // can be null
+		Env env = null; // can be null
 		try {
-			HttpServer server = new HttpServerFactory().get(modules, env, database);
+			HttpServer server = new HttpServerFactory()
+					// calling settings...
+					.get(modules, env, database);
 			
 			/* start */
 			if (database != null) {
 				database.createDbAndMigrate();
 			}
-			server.start();
+			server.start();// start IS NOT BLOCKING
 			
 			/* stop */
 			server.stop();
--- src/test/java/samples/getstarted/init/QuickInit.java
+++ src/test/java/samples/getstarted/init/QuickInit.java
@@ -5,13 +5,19 @@ import java.util.Arrays;
 import toti.Application;
 
 public class QuickInit {
-
+	
 	public static void main(String[] args) {
+		// Application.APP_CONFIG_FILE = "";
 		Application application = new Application(Arrays.asList(
 			// modules
 		));
 		
-		// stop zastavi celou aplikaci, chyba pri startu taky
+		// if start fail, System.exit is called
+		// start() IS NOT BLOCKING
+		application.start();
+		
+		// if stop fail, System.exit is called
+		application.stop();
 	}
 	
 }
