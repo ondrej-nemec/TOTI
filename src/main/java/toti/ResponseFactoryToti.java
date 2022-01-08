e29835e397ac95737b6dffd4dd1ab96625920370
Ondřej Němec <nikdynikde@gmail.com>
2022-01-08 23:48:25 +0100

    resp fac toti returns restapirespose

--- src/main/java/toti/ResponseFactoryToti.java
+++ src/main/java/toti/ResponseFactoryToti.java
@@ -3,41 +3,73 @@ package toti;
 import java.util.HashMap;
 import java.util.List;
 
+import ji.common.exceptions.NotImplementedYet;
 import ji.common.structures.MapInit;
 import ji.socketCommunication.http.HttpMethod;
 import ji.socketCommunication.http.StatusCode;
 import ji.socketCommunication.http.server.RequestParameters;
+import ji.socketCommunication.http.server.RestApiResponse;
+import ji.translator.Translator;
 import toti.profiler.Profiler;
 import toti.response.Response;
 import toti.security.Identity;
+import toti.templating.TemplateFactory;
 
 public class ResponseFactoryToti {
 	
 	private final Profiler profiler;
 //	private final DbViewerRouter dbViewer;
 	private final List<String> developIps;
+	private final TemplateFactory templateFactory;
+	private final Translator translator;
+	private final String charset;
 	
-	public ResponseFactoryToti(Profiler profiler, List<String> developIps) {
+	public ResponseFactoryToti(
+			Profiler profiler, List<String> developIps,
+			Translator translator, TemplateFactory templateFactory,
+			String charset) {
 		this.profiler = profiler;
 		this.developIps = developIps;
+		this.templateFactory = templateFactory;
+		this.translator = translator;
+		this.charset = charset;
 	}
 
-	public Response getTotiResponse(HttpMethod method, String url, RequestParameters params, Identity identity, ResponseHeaders headers) {
-		if (url.length() < 2 && developIps.contains(identity.getIP())) {// "/toti"->"" OR "/toti/"->"/"
+	public RestApiResponse getTotiResponse(HttpMethod method, String url, RequestParameters params, Identity identity, ResponseHeaders headers) {
+		return getResponse(method, url, params, identity, headers)
+			.getResponse(
+				headers, templateFactory, translator.withLocale(identity.getLocale()),
+				null /*authorizator*/, identity, null /* mapped url */, charset
+			);
+	}
+	
+	private Response getResponse(HttpMethod method, String url, RequestParameters params, Identity identity, ResponseHeaders headers) {
+		if ((url.length() < 2 || url.equals("/index.html")) && developIps.contains(identity.getIP())) {// "/toti"->"" OR "/toti/"->"/"
 			return getWelcomePage();
 		}
-		/*
-		if (url.substring(6).startsWith("db")) {
-			return dbViewer.getResponse(method, url.substring(8), params, identity, headers);
+		if (url.startsWith("db")) {
+			return getDbViewer(method, url, params, identity, headers);
 		}
-		*/
 		if (url.startsWith("/profiler")) {
-			if (profiler.isUse() && developIps.contains(identity.getIP())) {
-				return profiler.getResponse(method, params);
-			}
-			return Response.getText(StatusCode.FORBIDDEN, "");
+			return getProfiler(method, params, identity);
 		}
-		return Response.getTemplate("/assets" + url.substring(5), new MapInit<String, Object>().append("useProfiler", profiler.isUse()).toMap());
+		return getTotiFiles(url);
+	}
+	
+	private Response getProfiler(HttpMethod method, RequestParameters params, Identity identity) {
+		if (profiler.isUse() && developIps.contains(identity.getIP())) {
+			return profiler.getResponse(method, params);
+		}
+		return Response.getText(StatusCode.FORBIDDEN, "");
+	}
+	
+	private Response getTotiFiles(String url) {
+		return Response.getTemplate("/assets" + url, new MapInit<String, Object>().append("useProfiler", profiler.isUse()).toMap());
+	}
+	
+	private Response getDbViewer(HttpMethod method, String url, RequestParameters params, Identity identity, ResponseHeaders headers) {
+		throw new NotImplementedYet();
+		// TODO return dbViewer.getResponse(method, url.substring(8), params, identity, headers);
 	}
 	
 	private Response getWelcomePage() {
