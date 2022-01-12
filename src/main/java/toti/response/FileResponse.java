cd0f638fbb9d59476a1c1ee60076dc3d92d7829e
Ondřej Němec <nikdynikde@gmail.com>
2022-01-13 00:29:06 +0100

    file response support for file name and generated file

--- src/main/java/toti/response/FileResponse.java
+++ src/main/java/toti/response/FileResponse.java
@@ -1,6 +1,12 @@
 package toti.response;
 
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import ji.common.exceptions.LogicException;
 import ji.common.functions.InputStreamLoader;
+import ji.common.structures.ThrowingConsumer;
 import ji.files.text.Binary;
 import ji.socketCommunication.http.StatusCode;
 import ji.socketCommunication.http.server.RestApiResponse;
@@ -15,6 +21,8 @@ public class FileResponse implements Response {
 
 	private final String fileName;
 	private final StatusCode code;
+	private ByteArrayOutputStream fileContent = null;
+	private String fromFile = null;
 	
 	public FileResponse(StatusCode code, String fileName) {
 		this.fileName = fileName;
@@ -23,6 +31,37 @@ public class FileResponse implements Response {
 
 	@Override
 	public void addParam(String name, Object value) {}
+	
+	public FileResponse setFromFile(String fromFile) {
+		if (fileContent != null) {
+			throw new LogicException("Cannot create response from file and generate at once.");
+		}
+		this.fromFile = fromFile;
+		return this;
+	}
+	
+	public FileResponse addContent(Object data) {
+		if (data == null) {
+			return this;
+		}
+		return addContent(data.toString().getBytes());
+	}
+	
+	public FileResponse addContent(byte[] data) {
+		if (fromFile != null) {
+			throw new LogicException("Cannot create response from file and generate at once.");
+		}
+		if (fileContent == null) {
+			fileContent = new ByteArrayOutputStream();
+		}
+		try {
+			fileContent.write(data);
+		} catch (IOException e) {
+			// ignored
+			e.printStackTrace();
+		}
+		return this;
+	}
 	
 	@Override
 	public RestApiResponse getResponse(
@@ -36,15 +75,30 @@ public class FileResponse implements Response {
 		if (head != null) {
 			header.addHeader(head);
 		}
-		return RestApiResponse.binaryResponse(code, header.getHeaders(), (bout)->{
+	//	header.addHeader("Content-Disposition: attachment; filename=\"" + fileName + "\"");
+		if (fileContent != null) {
+			header.addHeader("Content-Disposition: inline; filename=\"" + fileName + "\"");
+			return RestApiResponse.binaryResponse(code, header.getHeaders(), (bout)->{
+				bout.write(fileContent.toString().getBytes());
+			});
+		}
+		if (fromFile != null) {
+			header.addHeader("Content-Disposition: inline; filename=\"" + fileName + "\"");
+			return RestApiResponse.binaryResponse(code, header.getHeaders(), binaryContent(fromFile));
+		}
+		return RestApiResponse.binaryResponse(code, header.getHeaders(), binaryContent(fileName));
+	}
+	
+	private ThrowingConsumer<BufferedOutputStream, IOException> binaryContent(String name) {
+		return (bout)->{
 			Binary.get().read((bin)->{
 				byte[] b = new byte[2048];
 				int len = 0;
 				while((len = bin.read(b)) != -1) {
 					bout.write(b, 0, len);
 				}
-			}, InputStreamLoader.createInputStream(getClass(), fileName));
-		});
+			}, InputStreamLoader.createInputStream(getClass(), name));
+		};
 	}
 
 }
