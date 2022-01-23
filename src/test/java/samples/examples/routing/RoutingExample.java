ca1e66365230e3fed079db59cf20deede1a268e7
Ondřej Němec <nikdynikde@gmail.com>
2022-01-23 13:05:32 +0100

    added routing example

---
+++ src/test/java/samples/examples/routing/RoutingExample.java
@@ -0,0 +1,236 @@
+package samples.examples.routing;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import ji.common.Logger;
+import ji.common.functions.Env;
+import ji.database.Database;
+import ji.translator.Translator;
+import toti.HttpServer;
+import toti.HttpServerFactory;
+import toti.Module;
+import toti.Router;
+import toti.annotations.Action;
+import toti.annotations.Controller;
+import toti.annotations.Param;
+import toti.annotations.ParamUrl;
+import toti.annotations.Secured;
+import toti.application.Task;
+import toti.register.Register;
+import toti.response.Response;
+import toti.url.Link;
+
+/**
+ * This example shows routing and link options
+ * @author Ondřej Němec
+ *
+ */
+@Controller("routing")
+public class RoutingExample implements Module {
+
+	/**
+	 * Secured route
+	 * Never show, always redirect
+	 * @return
+	 *  http://localhost:8080/examples/routing/secured
+	 *  OR with another pattern
+	 *  http://localhost:8080/api/routing/secured
+	 */
+	@Action("secured")
+	@Secured(isApi = false)
+	public Response secured() {
+		return Response.getText("Secured");
+	}
+	
+	/**
+	 * Unsecured route for redirect
+	 * @return
+	 *  http://localhost:8080/examples/routing/unsecured
+	 *  OR with another pattern
+	 *  http://localhost:8080/api/routing/unsecured
+	 */
+	@Action("unsecured")
+	public Response unsecured(@Param("backlink") String backLink) {
+		return Response.getText("Unsecured " + backLink);
+	}
+	
+	/**
+	 * This route is be overrrided in Router
+	 * @return 
+	 *  http://localhost:8080/examples/routing/notAccessible
+	 *  OR with another pattern
+	 *  http://localhost:8080/api/routing/notAccessible
+	 */
+	@Action("notAccessible")
+	public Response notAccessibleMethod() {
+		return Response.getText("This never appear");
+	}
+	
+	/**
+	 * This route is accessible
+	 * @return
+	 *  http://localhost:8080/examples/routing/accessible
+	 *  http://localhost:8080/examples/routing/notAccessible
+	 *  http://localhost:8080
+	 *  OR with another pattern
+	 *  http://localhost:8080/api/routing/accessible
+	 *  http://localhost:8080/api/routing/notAccessible
+	 *  http://localhost:8080
+	 */
+	@Action("accessible")
+	public Response accessibleMethod() {
+		return Response.getText("This always appear");
+	}
+	
+	/**
+	 * Display result of varios generated links
+	 * @return 
+	 *  http://localhost:8080/examples/routing/links
+	 *  OR with another pattern
+	 *  http://localhost:8080/api/routing/links
+	 */
+	@Action("links")
+	public Response links() {
+		Map<String, Object> params = new HashMap<>();
+		
+		// method
+		params.put(
+			"method",
+			Link.get()
+				.setMethod("accessibleMethod")
+				.create()
+		);
+		// method, parameter
+		params.put(
+			"methodParam",
+			Link.get()
+				.setMethod("unsecured")
+				.addGetParam("backlink", "/back/link")
+				.create()
+		);
+		// method, url parameter
+		params.put(
+			"methodUrlParam",
+			Link.get()
+				.setMethod("linksDestination")
+				.addUrlParam(42)
+				.addUrlParam("john-smith")
+				.create()
+		);
+		// method and controller
+		params.put(
+			"methodController",
+			Link.get()
+				.setController(RoutingExample.class)
+				.setMethod("accessibleMethod")
+				.create()
+		);
+		// method, controller, module
+		params.put(
+			"methodControllerModule",
+			Link.get()
+				.setModule(RoutingExample.class)
+				.setController(RoutingExample.class)
+				.setMethod("accessibleMethod")
+				.create()
+		);
+		// method calling
+		params.put(
+			"calling",
+			Link.get()
+				.create(RoutingExample.class, c->c.accessibleMethod())
+		);
+		// method calling parameters
+		params.put(
+			"callingParameter",
+			Link.get()
+				.addGetParam("backlink", "/back/link")
+				.create(RoutingExample.class, c->c.unsecured(null))
+		);
+		// method calling url parameters
+		params.put(
+			"callingUrlParameter",
+			Link.get()
+				.addGetParam("get", "getParam")
+				.addUrlParam(42)
+				.addUrlParam("john-smith")
+				.create(RoutingExample.class, c->c.linksDestination(null, null))
+		);
+		
+		return Response.getTemplate(
+			"/links.jsp",
+			params
+		);
+	}
+
+	/**
+	 * Used for Link::create testing
+	 * @return
+	 */
+	@Action("links-destination")
+	public Response linksDestination(@ParamUrl("id") Integer id, @ParamUrl("name") String name) {
+		return Response.getText("This never appear");
+	}
+	
+	@Override
+	public void addRoutes(Router router) {
+		// set URL for redirect if not logged in user try access secured route
+		router.setRedirectOnNotLoggedInUser(Link.get().create(RoutingExample.class, c->c.unsecured(null)));
+		
+		// replace empty route with 'accessible'
+		router.addUrl("", Link.get().create(RoutingExample.class, c->c.accessibleMethod()));
+
+		// replace 'notAccessible' route with 'accessible'
+		router.addUrl(
+			Link.get().create(RoutingExample.class, c->c.notAccessibleMethod()), 
+			Link.get().create(RoutingExample.class, c->c.accessibleMethod())
+		);
+	}
+	
+	public static void main(String[] args) {
+		List<Module> modules = Arrays.asList(new RoutingExample());
+		try {
+			HttpServer server = new HttpServerFactory()
+				.setPort(8080)
+				.setUrlPattern("/api/[controller]/[method]</[param]></[param]>")
+				.get(modules, null, null);
+			
+			/* start */
+			server.start();
+	
+			// sleep for 2min before automatic close
+			try { Thread.sleep(2 * 60 * 1000); } catch (InterruptedException e) { e.printStackTrace(); }
+			
+			/* stop */
+			server.stop();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.exit(0);
+	}
+
+	@Override
+	public String getName() {
+		return "examples";
+	}
+
+	@Override
+	public String getControllersPath() {
+		return "samples/examples/routing";
+	}
+	
+	@Override
+	public String getTemplatesPath() {
+		return "samples/examples/routing";
+	}
+
+	@Override
+	public List<Task> initInstances(Env env, Translator translator, Register registr, Database database, Logger logger)
+			throws Exception {
+		registr.addFactory(RoutingExample.class, ()->new RoutingExample());
+		return Arrays.asList();
+	}
+}
---
+++ src/test/java/samples/examples/routing/links.jsp
@@ -0,0 +1,41 @@
+<html>
+<head>
+	<title>Routing example - gerated links</title>
+</head>
+<body>
+	<table>
+		<tr>
+			<th>Just method</th>
+			<td>${method}</td>
+		</tr>
+		<tr>
+			<th>With parameter</th>
+			<td>${methodParam}</td>
+		</tr>
+		<tr>
+			<th>With URL parameters</th>
+			<td>${methodUrlParam}</td>
+		</tr>
+		<tr>
+			<th>Method and controller</th>
+			<td>${methodController}</td>
+		</tr>
+		<tr>
+			<th>Method, controller and module</th>
+			<td>${methodControllerModule}</td>
+		</tr>
+		<tr>
+			<th>Method calling</th>
+			<td>${calling}</td>
+		</tr>
+		<tr>
+			<th>Method calling with parameter</th>
+			<td>${callingParameter}</td>
+		</tr>
+		<tr>
+			<th>Method calling with URL parameter</th>
+			<td>${callingUrlParameter}</td>
+		</tr>
+	</table>
+</body>
+</html>
