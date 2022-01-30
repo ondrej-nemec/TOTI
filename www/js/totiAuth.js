711bcb3f0968b43d015b19596c3497570cc31537
Ondřej Němec <nikdynikde@gmail.com>
2022-01-30 21:53:56 +0100

    toti auth v1

--- www/js/totiAuth.js
+++ www/js/totiAuth.js
@@ -1,93 +1,71 @@
-/* TOTI Auth version 0.0.4 */
+/* TOTI Auth version 1.0.0 */
 var totiAuth = {
-	variableName: "authentication",
-	getAuthHeader: function(access = true) {
-		var token = totiStorage.getVariable(totiAuth.variableName);
+	variableToken: "authentication",
+	variableConfig: "authentication",
+	getAuthHeader: function() {
+		var token = totiStorage.getVariable(totiAuth.variableToken);
 		if (token === null) {
 			return {};
 		}
 		return {
-			"Authorization": token.token_type + " " + (access ? token.access_token : token.refresh_token)
+			"Authorization": "bearer " + token.access_token
 		};
 	},
 	/* for public use */
 	getToken: function() {
-		var token = totiStorage.getVariable(totiAuth.variableName);
-		if (token !== null) {
-			delete token.config;
-		}
-		return token;
+		return totiStorage.getVariable(totiAuth.variableToken);
 	},
-	isRefreshActive: false,
-	setTokenRefresh: function(token = null, period = -1) {
-		if (totiAuth.isRefreshActive) {
-			console.log("Another refresh is running");
-			return false;
-		}
-		token = token || totiStorage.getVariable(totiAuth.variableName);
-		if (!token) {
-			console.log("No saved token");
-			return false;
-		}
-		totiAuth.isRefreshActive = true;
-		totiStorage.saveVariable(totiAuth.variableName, token);
-		if (period < 0) {
-			period = token.expires_in * 2 / 3;
-		}
-		var profiler = (typeof totiProfiler === 'undefined' ? {} : totiProfiler.getProfilerHeader());
-		setTimeout(function() {
-			totiLoad.async(
-				token.config.refresh.url,
-				token.config.refresh.method, 
-				{}, 
-				{
-					...totiAuth.getAuthHeader(false),
-					...profiler
-				}, 
-				function(gettedToken) {
-					totiAuth.isRefreshActive = false;
-					gettedToken.config = token.config;
-					totiAuth.setTokenRefresh(gettedToken, -1);
-				}, 
-				function(xhr) {
-					totiAuth.isRefreshActive = false;
-					/*if (period < 5000) {*/
-						totiStorage.removeVariable(totiAuth.variableName);
-					/*} else {
-						totiAuth.setTokenRefresh(token, period / 2);
-					}	*/
-					location.reload();			
-				}
-			);
-		}, period);
-		return true;
-	},
-	logout: function() {
-		if (totiStorage.getVariable(totiAuth.variableName) === null) {
-			console.log("No token");
-			return;
-		}
-		var token = totiStorage.getVariable(totiAuth.variableName);
+	logout: function(url, method) {
+		totiStorage.removeVariable(totiAuth.variableConfig);
+		totiStorage.removeVariable(totiAuth.variableToken);
 		totiLoad.async(
-			token.config.logout.url,
-			token.config.logout.method, 
+			url,
+			method, 
 			{}, 
-			{
-				...totiAuth.getAuthHeader(),
-				...totiProfiler.getProfilerHeader()
-			}, 
+			totiLoad.getHeaders(), 
 			function(res) {}, 
 			function(xhr, a, error) {
 				console.log(xhr, a, error);
 			}
 		);
-		totiStorage.removeVariable(totiAuth.variableName);
 	},
-	login: function(token, config) {
-		token.config = config;
-		totiAuth.setTokenRefresh(token);
+	login: function(token, config = null) {
+		totiStorage.saveVariable(totiAuth.variableToken, token);
+		if (config !== null) {
+			totiStorage.saveVariable(totiAuth.variableConfig, config);
+			totiAuth.setTokenRefresh();
+		}
 	},
-	onLoad: function() {
-		totiAuth.setTokenRefresh(null, 0);
+	isRefreshActive: false,
+	setTokenRefresh: function() {
+		if (totiAuth.isRefreshActive) {
+			console.log("Another refresh is running");
+			return false;
+		}
+		var config = totiStorage.getVariable(totiAuth.variableConfig);
+		if (!config) {
+			console.log("No saved config");
+			return false;
+		}
+		totiAuth.isRefreshActive = true;
+		setTimeout(function() {
+			totiLoad.async(
+				config.url,
+				config.method, 
+				{}, 
+				totiLoad.getHeaders(), 
+				function() {
+					totiAuth.isRefreshActive = false;
+					totiAuth.setTokenRefresh();
+				}, 
+				function(xhr) {
+					totiAuth.isRefreshActive = false;
+					totiAuth.logout();
+					location.reload();			
+				}
+			);
+		}, token.expires_in * 2 / 3);
+		return true;
 	}
 };
+totiAuth.setTokenRefresh();
