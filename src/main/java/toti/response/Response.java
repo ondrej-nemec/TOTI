ec373e19d1b0398168d6b54d84ce274354f8ddac
Ondřej Němec <nikdynikde@gmail.com>
2022-01-31 22:37:23 +0100

    code style

--- src/main/java/toti/response/Response.java
+++ src/main/java/toti/response/Response.java
@@ -35,70 +35,173 @@ public interface Response {
 	
 	/***********/
 	
+	/**
+	 * Very simple file response. Downloaded file name will be last URL parameter. Useful for HTML/CSS/JS resources.
+	 * @param fileName specify path to file anywhere on disk
+	 * @return
+	 */
 	static Response getFile(String fileName) {
 		return new FileResponse(StatusCode.OK, fileName);
 	}
 
+	/**
+	 * Very simple file response. Downloaded file name will be last URL parameter. Useful for HTML/CSS/JS resources.
+	 * @param code Response code
+	 * @param fileName specify path to file anywhere on disk
+	 * @return
+	 */
 	static Response getFile(StatusCode code, String fileName) {
 		return new FileResponse(code, fileName);
 	}
 	
+	/**
+	 * File response with headers for downloading.
+	 * @param sourceFile file from anywhere on disk
+	 * @param fileName specify downloaded file name
+	 * @return
+	 */
 	static Response getFileDownload(String sourceFile, String fileName) {
 		return new FileResponse(StatusCode.OK, fileName, sourceFile);
 	}
 
+	/**
+	 * File response with headers for downloading.
+	 * @param code Response code
+	 * @param sourceFile file from anywhere on disk
+	 * @param fileName specify downloaded file name
+	 * @return
+	 */
 	static Response getFileDownload(StatusCode code, String sourceFile, String fileName) {
 		return new FileResponse(code, fileName, sourceFile);
 	}
 	
+	/**
+	 * Allow create file content dynamically at runtime
+	 * @param fileName specify downloaded file name
+	 * @param binaryContent
+	 * @return
+	 */
 	static Response getFileDownload(String fileName, ThrowingConsumer<BufferedOutputStream, IOException> binaryContent) {
 		return new FileResponse(StatusCode.OK, fileName, binaryContent);
 	}
 
+	/**
+	 * Allow create file content dynamically at runtime
+	 * @param code Response code
+	 * @param fileName specify downloaded file name
+	 * @param binaryContent
+	 * @return
+	 */
 	static Response getFileDownload(StatusCode code, String fileName, ThrowingConsumer<BufferedOutputStream, IOException> binaryContent) {
 		return new FileResponse(code, fileName, binaryContent);
 	}
 	
+	/**
+	 * Send given object as JSON. JI Common Mapper is used for stringify.
+	 * @param code
+	 * @param json
+	 * @return
+	 */
 	static Response getJson(StatusCode code, Object json) {
 		return new JsonResponse(code, json);
 	}
 	
+	/**
+	 * Send given object as JSON. JI Common Mapper is used for stringify.
+	 * @param json
+	 * @return
+	 */
 	static Response getJson(Object json) {
 		return new JsonResponse(StatusCode.ACCEPTED, json);
 	}
 	
+	/**
+	 * Returns given text as response
+	 * @param text
+	 * @return
+	 */
 	static Response getText(String text) {
 		return new TextResponse(StatusCode.OK, text);
 	}
 	
+	/**
+	 * Returns given text as response
+	 * @param code
+	 * @param text
+	 * @return
+	 */
 	static Response getText(StatusCode code, String text) {
 		return new TextResponse(code, text);
 	}
 	
+	/**
+	 * Parse file as TOTI template with given parameters
+	 * @param code
+	 * @param fileName path to template files
+	 * @param params template paramters
+	 * @return
+	 */
 	static Response getTemplate(StatusCode code, String fileName, Map<String, Object> params) {
 		return new TemplateResponse(code, fileName, params);
 	}
 	
+	/**
+	 * Parse file as TOTI template with given parameters
+	 * @param fileName path to template files
+	 * @param params template paramters
+	 * @return
+	 */
 	static Response getTemplate(String fileName, Map<String, Object> params) {
 		return new TemplateResponse(StatusCode.OK, fileName, params);
 	}
 	
+	/**
+	 * Redirect to given URI. URI must be relative inside app.
+	 * @param code
+	 * @param url - relative path inside application
+	 * @return
+	 */
 	static Response getRedirect(StatusCode code, String url) {
 		return new RedirectResponse(code, url, false);
 	}
 	
+	/**
+	 * Redirect to given URI. URI must be relative inside app.
+	 * @param url - relative path inside application
+	 * @return
+	 */
 	static Response getRedirect(String url) {
 		return new RedirectResponse(StatusCode.TEMPORARY_REDIRECT, url, false);
 	}
 	
+	/**
+	 * Redirect to given URL. Can cause Open redirection vulnerability
+	 * @param code
+	 * @param url
+	 * @param allowOutOfAppRedirect - if can redirect out of application
+	 * @return
+	 */
 	static Response getRedirect(StatusCode code, String url, boolean allowOutOfAppRedirect) {
 		return new RedirectResponse(code, url, allowOutOfAppRedirect);
 	}
 	
+	/**
+	 * Redirect to given URL. Can cause Open redirection vulnerability
+	 * @param url
+	 * @param allowOutOfAppRedirect - if can redirect out of application
+	 * @return
+	 */
 	static Response getRedirect(String url, boolean allowOutOfAppRedirect) {
 		return new RedirectResponse(StatusCode.TEMPORARY_REDIRECT, url, allowOutOfAppRedirect);
 	}
 	
+	/**
+	 * For websocket response
+	 * @param websocket
+	 * @param onMessage
+	 * @param onError
+	 * @return
+	 */
 	static Response getWebsocket(WebSocket websocket, Consumer<String> onMessage, Consumer<IOException> onError) {
 		return new WebsocketResponse(websocket, onMessage, onError);
 	}
