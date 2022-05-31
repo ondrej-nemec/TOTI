fe3f6a339c947d304b6516f3cad9522d9e587da3
Ondřej Němec <nikdynikde@gmail.com>
2022-05-31 23:54:23 +0200

    lang cookie is same=strict

--- src/main/resources/toti/web/assets/js/totiLang.js
+++ src/main/resources/toti/web/assets/js/totiLang.js
@@ -1,9 +1,9 @@
-/* TOTI Lang version 0.0.2 */
+/* TOTI Lang version 0.0.3 */
 var totiLang = {
 	variableName: "language",
 	changeLanguage: function (language) {
 		totiStorage.saveVariable(totiLang.variableName, language);
-		document.cookie = "Language=" + language + ";Path=/";
+		document.cookie = "Language=" + language + ";Path=/; SameSite=Strict";
 	},
 	getLang: function() {
 		var lang = totiStorage.getVariable(totiLang.variableName);
