ba3e25be728a1f15f08fedb8df5b99a8765a27cb
Ondřej Němec <nikdynikde@gmail.com>
2022-08-02 21:14:57 +0200

    href with absolute path

--- src/main/java/toti/templating/parameters/HrefParameter.java
+++ src/main/java/toti/templating/parameters/HrefParameter.java
@@ -11,7 +11,12 @@ public class HrefParameter implements Parameter {
 
 	@Override
 	public String getCode(String value) {
-        return String.format("Link.get().create(\"%s\")", value);
+		String prefix = "";
+        if (value != null && value.startsWith("A:")) {
+             prefix = value.substring(2, value.indexOf(":", 3));
+             value = value.substring(value.indexOf(":", 3)+1);
+        }
+        return String.format("\"%s\" + Link.get().create(\"%s\")", prefix, value);
         // return "\"" + link.create() + "\"";
     }
 
