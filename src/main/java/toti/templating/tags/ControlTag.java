9be6d9c4ae1faf629be62ab1026f7491d3f5d593
Ondřej Němec <ondrej.nemec@autmes.cz>
2022-11-02 10:11:35 +0100

    control tag fix

--- src/main/java/toti/templating/tags/ControlTag.java
+++ src/main/java/toti/templating/tags/ControlTag.java
@@ -23,7 +23,16 @@ public class ControlTag implements Tag {
 
 	@Override
 	public String getPairEndCode(Map<String, String> params) {
-		return "write(\"</div>\");";
+		StringBuilder code = new StringBuilder("flushNode();});");
+		code.append("getBlock(\"totiLastControl\", false)");
+		code.append(".accept(new MapInit<String, Object>()");
+		params.forEach((name, value)->{
+			if (!name.equals("name") && !name.equals("optional") && !name.equals("form") && !name.equals("grid")) {
+				code.append(String.format(".append(\"%s\", \"%s\")", name, value));
+			}
+		});
+		code.append(".toMap());");
+		return code.toString();
 	}
 
 	@Override
@@ -38,46 +47,41 @@ public class ControlTag implements Tag {
 		} else if (params.containsKey("grid")) {
 			type = "Grid";
 		}
-		// TODO nebude uplne fungovat pri sdilenem tagu
-		StringBuilder code = new StringBuilder("write(\"<div id='control-:name:'");
-		params.forEach((k, v)->{
-			switch (k) {
-				case "optional":
-				case "form":
-				case "grid":
-				case "name":
-				case "id":
-					break;
-				default:
-					code.append(String.format(" %s='%s'", k, v));
-					break;
-			}
-		});
-		code.append(" >\");");
+		StringBuilder code = new StringBuilder();
+		code.append("addBlock(\"totiLastControl\", (%sParams)->{");
+		code.append("initNode(%sParams);");
 		code.append(
-			"{"
-				+ "if(variables.get(\":name:\") == null && !%s) {throw new TemplateException(\"Tag Missing control variable: ':name:'\");}"
-				+ "if(variables.get(\":name:\") != null) {"
-					+ "toti.control.Control control=(toti.control.Control)(getVariable(\":name:\"));"
-					+ "if(\"%s\".isEmpty() || control.getType().equals(\"%s\")){"
-						+ "write(\""
-						+ "<script> {"
-							+ "window.addEventListener('load', ()=> {"
-								+ "try { "
-									+ "\"+control.toString()+\""
-									+ ".render('div#control-:name:', 'toti-\" + control.getType() + \"-:name:'"
-									+ (useDefaultTemplate ? "" : ", false")
-									+ ");"
-								+ " } catch(e) { console.error(e); }"
-							+ "});"
-						+ "} </script>"
-						+ "\");"
-					+ "}"
-				+ "}"
+			"if(variables.get(\":name:\") == null && !%s) {"
+					+ "throw new TemplateException(\"Tag Missing control variable: ':name:'\");"
 			+ "}"
+			+ "if(variables.get(\":name:\") == null) {"
+				+ "flushNode();"
+				+ "return;"
+			+ "}"
+			+ "toti.control.Control control=(toti.control.Control)(getVariable(\":name:\"));"
+			+ "if(!\"%s\".isEmpty() && !control.getType().equals(\"%s\")){"
+				+ "flushNode();"
+				+ "return;"
+			+ "}"
+			+ "write(\"<div id='control-:name:' >\");"
+			+ "write(\""
+				+ "<script> {"
+					+ "window.addEventListener('load', ()=> {"
+						+ "try { "
+							+ "\"+control.toString()+\""
+							+ ".render('div#control-:name:', 'toti-\" + control.getType() + \"-:name:'"
+							+ (useDefaultTemplate ? "" : ", false")
+							+ ");"
+						+ " } catch(e) { console.error(e); }"
+					+ "});"
+				+ "} </script>"
+			+ "\");"
 		);
+		String paramsName = params.get("name").replace("-", "_");
 		return String.format(
 			code.toString().replace(":name:", params.get("name")),
+			paramsName,
+			paramsName,
 			params.containsKey("optional"),
 			type,
 			type
