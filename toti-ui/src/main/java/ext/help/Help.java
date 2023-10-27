92b0ed89effc39311185959377f194e6dafad7a3
Ondřej Němec <nikdynikde@gmail.com>
2023-10-27 21:53:44 +0200

    help allows send another parameters

--- toti-ui/src/main/java/ext/help/Help.java
+++ toti-ui/src/main/java/ext/help/Help.java
@@ -1,5 +1,8 @@
 package ext.help;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import ji.json.Jsonable;
 
 public class Help implements Jsonable {
@@ -8,6 +11,7 @@ public class Help implements Jsonable {
 	private final Object title;
 	private final String optgroup;
 	private final boolean disabled;
+	private final Map<String, Object> params;
 	
 	public Help(Object value, Object title) {
 		this(value, title, null, false);
@@ -18,6 +22,7 @@ public class Help implements Jsonable {
 		this.title = title;
 		this.optgroup = optgroup;
 		this.disabled = disabled;
+		this.params = new HashMap<>();
 	}
 
 	public Object getValue() {
@@ -35,10 +40,25 @@ public class Help implements Jsonable {
 	public boolean isDisabled() {
 		return disabled;
 	}
+	
+	public Help addParameter(String name, Object value) {
+		this.params.put(name, value);
+		return this;
+	}
 
 	@Override
 	public String toString() {
 		return "Help [value=" + value + ", title=" + title + ", optgroup=" + optgroup + ", disabled=" + disabled + "]";
+	}
+	
+	@Override
+	public Object toJson() {
+		Map<String, Object> params = new HashMap<>(this.params);
+		params.put("value", value);
+		params.put("title", title);
+		params.put("optgroup", optgroup);
+		params.put("disabled", disabled);
+		return params;
 	}
 	
 }
