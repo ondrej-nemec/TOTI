ce5cf8718b32e297e01adb78f959895f290176a6
Ondřej Němec <nikdynikde@gmail.com>
2023-11-30 19:11:09 +0100

    help added equals

--- toti-ui/src/main/java/toti/ui/backend/help/Help.java
+++ toti-ui/src/main/java/toti/ui/backend/help/Help.java
@@ -60,5 +60,63 @@ public class Help implements Jsonable {
 		params.put("disabled", disabled);
 		return params;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (disabled ? 1231 : 1237);
+		result = prime * result + ((optgroup == null) ? 0 : optgroup.hashCode());
+		result = prime * result + ((params == null) ? 0 : params.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Help other = (Help) obj;
+		if (disabled != other.disabled) {
+			return false;
+		}
+		if (optgroup == null) {
+			if (other.optgroup != null) {
+				return false;
+			}
+		} else if (!optgroup.equals(other.optgroup)) {
+			return false;
+		}
+		if (params == null) {
+			if (other.params != null) {
+				return false;
+			}
+		} else if (!params.equals(other.params)) {
+			return false;
+		}
+		if (title == null) {
+			if (other.title != null) {
+				return false;
+			}
+		} else if (!title.equals(other.title)) {
+			return false;
+		}
+		if (value == null) {
+			if (other.value != null) {
+				return false;
+			}
+		} else if (!value.equals(other.value)) {
+			return false;
+		}
+		return true;
+	}
 	
 }
