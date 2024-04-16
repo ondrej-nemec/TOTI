09a77c9f5257166b1b97823827cf08a8d3d36a2c
Ondřej Němec <nikdynikde@gmail.com>
2024-04-16 21:05:38 +0200

    transaction listener throws SQLException

--- toti-extensions/te-ui/src/main/java/toti/ui/backend/TransactionListener.java
+++ toti-extensions/te-ui/src/main/java/toti/ui/backend/TransactionListener.java
@@ -1,9 +1,11 @@
 package toti.ui.backend;
 
+import java.sql.SQLException;
+
 public interface TransactionListener<S> {
 
-	default void onTransactionStart(Object id, S item) {}
+	default void onTransactionStart(Object id, S item) throws SQLException {}
 
-	default void onTransactionEnd(Object id, S item) {}
+	default void onTransactionEnd(Object id, S item) throws SQLException {}
 	
 }
