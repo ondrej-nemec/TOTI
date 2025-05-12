1b1da5dc5a0398060d8e099423effbcbabd46362
Ondřej Němec <nikdynikde@gmail.com>
2025-05-12 21:18:18 +0200

    test case with assert* methods

--- toti-libs/toti-db-testing/src/main/java/ji/testing/TestCase.java
+++ toti-libs/toti-db-testing/src/main/java/ji/testing/TestCase.java
@@ -1,9 +1,12 @@
 package ji.testing;
 
+import java.util.Collection;
+import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+import ji.common.functions.Implode;
 import ji.common.structures.ThrowingFunction;
 
 public interface TestCase {
@@ -22,5 +25,56 @@ public interface TestCase {
 
 	default <T, R, E extends Exception> ThrowingFunction<T, R, E> throwingFunction(ThrowingFunction<T, R, E> function) {
 		return function;
+	}
+
+	default void assertEquals(Map<?, ?> expected, Map<?, ?> actual) {
+		assertEquals(null, expected, actual);
+	}
+	
+	default void assertEquals(String message, Map<?, ?> expected, Map<?, ?> actual) {
+		try {
+			assertEquals(message, expected, actual);
+		} catch (Error e) {
+			assertEquals(
+				message,
+				expected == null ? "NULL" : Implode.implode("\n", ":", expected),
+				actual == null ? "NULL" : Implode.implode("\n", ":", actual)
+			);
+			throw e;
+		}
+	}
+	
+	default void assertEquals(Collection<?> expected, Collection<?> actual) {
+		assertEquals(null, expected, actual);
+	}
+
+	default void assertEquals(String message, Collection<?> expected, Collection<?> actual) {
+		try {
+			assertEquals(message, expected, actual);
+		} catch (Error e) {
+			assertEquals(
+				message,
+				expected == null ? "NULL" : Implode.implode("\n", expected),
+				actual == null ? "NULL" : Implode.implode("\n", actual)
+			);
+			throw e;
+		}
+	}
+
+	default void assertEquals(Object expected, Object actual) {
+		assertEquals(null, expected, actual);
+	}
+
+	default void assertEquals(String message, Object expected, Object actual) {
+		try {
+			assertEquals(message, expected, actual);
+		} catch (Error e) {
+			assertEquals(
+				message,
+				expected == null ? "NULL" : expected.toString(),
+				actual == null ? "NULL" : expected.toString()
+			);
+			throw e;
+		}
 	}
 }
