b72cd90f5bff1537c655ee511fddc563f54bc2ac
Ondřej Němec <nikdynikde@gmail.com>
2026-06-15 20:02:38 +0000

    TestCase:_toString with flag for class

--- toti-libs/toti-common/src/test/java/toti/lib/common/tests/TestCaseTest.java
+++ toti-libs/toti-common/src/test/java/toti/lib/common/tests/TestCaseTest.java
@@ -1,7 +1,6 @@
 package toti.lib.common.tests;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,30 +13,30 @@ public class TestCaseTest {
 
 	@ParameterizedTest
 	@MethodSource
-	public void testToString_map(String prefix, Map<?, ?> map, String expected) {
-		assertEquals(expected, TestCase._toString(map, prefix));
+	public void testToString_map(String prefix, boolean withClass, Map<?, ?> map, String expected) {
+		assertEquals(expected, TestCase._toString(map, prefix, withClass));
 	}
 
 	public static Object[] testToString_map() {
 		return new Object[] {
 			new Object[] {
-				"", null, "NULL"
+				"", false, null, "NULL"
 			},
 			new Object[] {
-				"", MapInit.create().toMap(), "{}"
+				"", false, MapInit.create().toMap(), "{}"
 			},
 			new Object[] {
-				"  ", MapInit.create().toMap(), "{}"
+				"  ", false, MapInit.create().toMap(), "{}"
 			},
 			new Object[] {
-				"", MapInit.create().append("a", "A").toMap(),
+				"", false, MapInit.create().append("a", "A").toMap(),
 """
 {
   a: A
 }"""
 			},
 			new Object[] {
-				"", MapInit.create().append("a", "A").append("b", "B").toMap(),
+				"", false, MapInit.create().append("a", "A").append("b", "B").toMap(),
 """
 {
   a: A
@@ -45,42 +44,44 @@ public class TestCaseTest {
 }"""
 			},
 			new Object[] {
-				"  ", MapInit.create().append("a", "A").append("b", "B").toMap(),
+				"  ", false, MapInit.create().append("a", "A").append("b", "B").toMap(),
 """
 {
     a: A
     b: B
   }"""
-			}
+			},
+			new Object[] {
+				"", true, null, "NULL"
+			},
+			new Object[] {
+				"", true, MapInit.create().append("a", "A").append("b", "B").toMap(),
+"""
+(java.util.HashMap) {
+  a: A (java.lang.String)
+  b: B (java.lang.String)
+}"""
+			},
 		};
 	}
 
+
 	@ParameterizedTest
 	@MethodSource
-	public void testToString_list(String prefix, List<?> list, String expected) {
-		assertEquals(expected, TestCase._toString(list, prefix));
+	public void testToString_array(String prefix, boolean withClass, Object[] array, String expected) {
+		assertEquals(expected, TestCase._toString(array, prefix, withClass));
 	}
 
-	public static Object[] testToString_list() {
+	public static Object[] testToString_array() {
 		return new Object[] {
 			new Object[] {
-				"", null, "NULL"
+				"", false, null, "NULL"
 			},
 			new Object[] {
-				"", Arrays.asList(), "[]"
+				"", false, new String[] {}, "[]"
 			},
 			new Object[] {
-				"  ", Arrays.asList(), "[]"
-			},
-			new Object[] {
-				"", Arrays.asList("a"),
-"""
-[
-  a
-]"""
-			},
-			new Object[] {
-				"", Arrays.asList("a", "b"),
+				"", false, new String[] {"a", "b"},
 """
 [
   a
@@ -88,41 +89,106 @@ public class TestCaseTest {
 ]"""
 			},
 			new Object[] {
-				"  ", Arrays.asList("a", "b"),
+				"  ", false, new String[] {"a", "b"}, 
 """
 [
     a
     b
   ]"""
-			}
+			},
+			new Object[] {
+				"", true, new String[] {"a", "b"},
+"""
+(java.util.Arrays.ArrayList) [
+  a (java.lang.String)
+  b (java.lang.String)
+]"""
+			},
+
 		};
 	}
 
 	@ParameterizedTest
 	@MethodSource
-	public void testToString_value(String prefix, Object value, String expected) {
-		assertEquals(expected, TestCase._toString(value, prefix));
+	public void testToString_list(String prefix, boolean withClass, Iterable<?> list, String expected) {
+		assertEquals(expected, TestCase._toString(list, prefix, withClass));
+	}
+
+	public static Object[] testToString_list() {
+		return new Object[] {
+			new Object[] {
+				"", false, null, "NULL"
+			},
+			new Object[] {
+				"", false, Arrays.asList(), "[]"
+			},
+			new Object[] {
+				"  ", false, Arrays.asList(), "[]"
+			},
+			new Object[] {
+				"", false, Arrays.asList("a"),
+"""
+[
+  a
+]"""
+			},
+			new Object[] {
+				"", false, Arrays.asList("a", "b"),
+"""
+[
+  a
+  b
+]"""
+			},
+			new Object[] {
+				"  ", false, Arrays.asList("a", "b"),
+"""
+[
+    a
+    b
+  ]"""
+			},
+			new Object[] {
+				"", true, null, "NULL"
+			},
+			new Object[] {
+				"", true, Arrays.asList(), "(java.util.Arrays.ArrayList) []"
+			},
+			new Object[] {
+				"", true, Arrays.asList("a"),
+"""
+(java.util.Arrays.ArrayList) [
+  a (java.lang.String)
+]"""
+			},
+		};
+	}
+
+	@ParameterizedTest
+	@MethodSource
+	public void testToString_value(String prefix, boolean withClass, Object value, String expected) {
+		assertEquals(expected, TestCase._toString(value, prefix, withClass));
 	}
 
 	public static Object[] testToString_value() {
 		return new Object[] {
 			new Object[] {
-				"", null, "NULL"
+				"", false, null, "NULL"
 			},
 			new Object[] {
-				"", 1, "1"
+				"", false, 1, "1"
 			},
 			new Object[] {
-				"", "aaa", "aaa"
+				"", false, "aaa", "aaa"
 			},
 			new Object[] {
-				"", "'aaa'", "'aaa'"
+				"", false, "'aaa'", "'aaa'"
 			},
 			new Object[] {
-				"", "\"aaa\"", "\"aaa\""
+				"", false, "\"aaa\"", "\"aaa\""
 			},
 			new Object[] {
-				"", Arrays.asList(
+				"", false, Arrays.asList(
 					MapInit.create().append("a", "A").append("b", "B").toMap(),
 					MapInit.create().append("c", "C").append("d", "D").toMap()
 				),
@@ -139,7 +205,7 @@ public class TestCaseTest {
 ]"""
 			},
 			new Object[] {
-				"  ", Arrays.asList(
+				"  ", false, Arrays.asList(
 					MapInit.create().append("a", "A").append("b", "B").toMap(),
 					MapInit.create().append("c", "C").append("d", "D").toMap()
 				),
@@ -156,7 +222,7 @@ public class TestCaseTest {
   ]"""
 			},
 			new Object[] {
-				"", MapInit.create()
+				"", false, MapInit.create()
 				.append("a", Arrays.asList("A1", "A2"))
 				.append("b", Arrays.asList("B1", "B2"))
 				.toMap(),
@@ -173,7 +239,7 @@ public class TestCaseTest {
 }"""
 			},
 			new Object[] {
-				"  ", MapInit.create()
+				"  ", false, MapInit.create()
 				.append("a", Arrays.asList("A1", "A2"))
 				.append("b", Arrays.asList("B1", "B2"))
 				.toMap(),
@@ -188,6 +254,32 @@ public class TestCaseTest {
       B2
     ]
   }"""
+			},
+			new Object[] {
+				"", true, null, "NULL"
+			},
+			new Object[] {
+				"", true, 1, "1 (java.lang.Integer)"
+			},
+			new Object[] {
+				"", true, "aaa", "aaa (java.lang.String)"
+			},
+			new Object[] {
+				"", true, Arrays.asList(
+					MapInit.create().append("a", "A").append("b", "B").toMap(),
+					MapInit.create().append("c", "C").append("d", "D").toMap()
+				),
+"""
+(java.util.Arrays.ArrayList) [
+  (java.util.HashMap) {
+    a: A (java.lang.String)
+    b: B (java.lang.String)
+  }
+  (java.util.HashMap) {
+    c: C (java.lang.String)
+    d: D (java.lang.String)
+  }
+]"""
 			},
 		};
 	}
--- toti-libs/toti-common/src/testFixtures/java/toti/lib/common/tests/TestCase.java
+++ toti-libs/toti-common/src/testFixtures/java/toti/lib/common/tests/TestCase.java
@@ -1,5 +1,6 @@
 package toti.lib.common.tests;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -38,106 +39,176 @@ public interface TestCase {
 	
 	/**************************/
 
+	//#region
+
 	static void assertEquals(Map<?, ?> expected, Map<?, ?> actual) {
-		assertEquals(expected, actual, null);
+		assertEquals(expected, actual, null, false);
 	}
 	
 	static void assertEquals(Map<?, ?> expected, Map<?, ?> actual, String message) {
+		assertEquals(expected, actual, message, false);
+	}
+
+	static void assertEquals(Map<?, ?> expected, Map<?, ?> actual, boolean withClass) {
+		assertEquals(expected, actual, null, withClass);
+	}
+	
+	static void assertEquals(Map<?, ?> expected, Map<?, ?> actual, String message, boolean withClass) {
 		try {
 			Assertions.assertEquals(expected, actual, message);
 		} catch (Error e) {
-			Assertions.assertEquals(_toString(expected), _toString(actual), message);
+			Assertions.assertEquals(_toString(expected, withClass), _toString(actual, withClass), message);
 			throw e;
 		}
 	}
 	
+	//#endregion
+	//#region
+
 	static void assertEquals(Collection<?> expected, Collection<?> actual) {
-		assertEquals(expected, actual, null);
+		assertEquals(expected, actual, null, false);
 	}
 
 	static void assertEquals(Collection<?> expected, Collection<?> actual, String message) {
+		assertEquals(expected, actual, message, false);
+	}
+
+	static void assertEquals(Collection<?> expected, Collection<?> actual, boolean withClass) {
+		assertEquals(expected, actual, null, withClass);
+	}
+
+	static void assertEquals(Collection<?> expected, Collection<?> actual, String message, boolean withClass) {
 		try {
 			Assertions.assertEquals(expected, actual, message);
 		} catch (Error e) {
-			Assertions.assertEquals(_toString(expected), _toString(actual), message);
+			Assertions.assertEquals(_toString(expected, withClass), _toString(actual, withClass), message);
 			throw e;
 		}
 	}
 
+	//#endregion
+	//#region
+
 	static void assertEquals(Object expected, Object actual) {
-		assertEquals(expected, actual, null);
+		assertEquals(expected, actual, null, false);
 	}
 
 	static void assertEquals(Object expected, Object actual, String message) {
+		assertEquals(expected, actual, message, false);
+	}
+
+	static void assertEquals(Object expected, Object actual, boolean withClass) {
+		assertEquals(expected, actual, null, withClass);
+	}
+
+	static void assertEquals(Object expected, Object actual, String message, boolean withClass) {
 		try {
 			Assertions.assertEquals(expected, actual, message);
 		} catch (Error e) {
-			Assertions.assertEquals(_toString(expected), _toString(actual), message);
+			Assertions.assertEquals(_toString(expected, withClass), _toString(actual, withClass), message);
 			throw e;
 		}
 	}
+
+	//#endregion
 	
 	/*********************************/
 
+	//#region
+
 	static void assertNotEquals(Map<?, ?> expected, Map<?, ?> actual) {
-		assertNotEquals(expected, actual, null);
+		assertNotEquals(expected, actual, null, false);
 	}
 	
 	static void assertNotEquals(Map<?, ?> expected, Map<?, ?> actual, String message) {
+		assertNotEquals(expected, actual, message, false);
+	}
+
+	static void assertNotEquals(Map<?, ?> expected, Map<?, ?> actual, boolean withClass) {
+		assertNotEquals(expected, actual, null, withClass);
+	}
+	
+	static void assertNotEquals(Map<?, ?> expected, Map<?, ?> actual, String message, boolean withClass) {
 		try {
 			Assertions.assertNotEquals(expected, actual, message);
 		} catch (Error e) {
-			Assertions.assertNotEquals(_toString(expected), _toString(actual), message);
+			Assertions.assertNotEquals(_toString(expected, withClass), _toString(actual, withClass), message);
 			throw e;
 		}
 	}
 	
+	//#endregion
+	//#region
+
 	static void assertNotEquals(Collection<?> expected, Collection<?> actual) {
-		assertEquals(expected, actual, null);
+		assertNotEquals(expected, actual, null, false);
 	}
 
 	static void assertNotEquals(Collection<?> expected, Collection<?> actual, String message) {
+		assertNotEquals(expected, actual, message, false);
+	}
+
+	static void assertNotEquals(Collection<?> expected, Collection<?> actual, boolean withClass) {
+		assertNotEquals(expected, actual, null, withClass);
+	}
+
+	static void assertNotEquals(Collection<?> expected, Collection<?> actual, String message, boolean withClass) {
 		try {
 			Assertions.assertNotEquals(expected, actual, message);
 		} catch (Error e) {
-			Assertions.assertNotEquals(_toString(expected), _toString(actual), message);
+			Assertions.assertNotEquals(_toString(expected, withClass), _toString(actual, withClass), message);
 			throw e;
 		}
 	}
 
+	//#endregion
+	//#region
+	
 	static void assertNotEquals(Object expected, Object actual) {
-		assertNotEquals(expected, actual, null);
+		assertNotEquals(expected, actual, null, false);
+	}
+	static void assertNotEquals(Object expected, Object actual, String message) {
+		assertNotEquals(expected, actual, message, false);
 	}
 
-	static void assertNotEquals(Object expected, Object actual, String message) {
+	static void assertNotEquals(Object expected, Object actual, boolean withClass) {
+		assertNotEquals(expected, actual, null, withClass);
+	}
+
+	static void assertNotEquals(Object expected, Object actual, String message, boolean withClass) {
 		try {
 			Assertions.assertNotEquals(expected, actual, message);
 		} catch (Error e) {
-			Assertions.assertNotEquals(_toString(expected), _toString(actual), message);
+			Assertions.assertNotEquals(_toString(expected, withClass), _toString(actual, withClass), message);
 			throw e;
 		}
 	}
+
+	//#endregion
 
 	/******************************/
 
-	static <K, V> String _toString(Map<K, V> map) {
-		return _toString(map, "");
+	static <K, V> String _toString(Map<K, V> map, boolean withClas) {
+		return _toString(map, "", withClas);
 	}
 
-	static <K, V> String _toString(Map<K, V> map, String prefix) {
+	static <K, V> String _toString(Map<K, V> map, String prefix, boolean withClass) {
 		if (map == null) {
 			return "NULL";
 		}
 		StringBuilder res = new StringBuilder();
-		//res.append(prefix);
-		res.append("{");
+		if (withClass) {
+			res.append(String.format("(%s) {", map.getClass().getCanonicalName()));
+		} else {
+			res.append("{");
+		}
 		map.forEach((k, v)->{
 			res.append("\n");
 			res.append(prefix);
 			res.append(PREFIX);
 			res.append(k);
 			res.append(": ");
-			res.append(_toString(v, prefix + PREFIX));
+			res.append(_toString(v, prefix + PREFIX, withClass));
 		});
 		if (!map.isEmpty()) {
 			res.append("\n");
@@ -147,23 +218,27 @@ public interface TestCase {
 		return res.toString();
 	}
 
-	static <T> String _toString(Iterable<T> iterable) {
-		return _toString(iterable, "");
+	static <T> String _toString(Iterable<T> iterable, boolean withClas) {
+		return _toString(iterable, "", withClas);
 	}
 
-	static <T> String _toString(Iterable<T> iterable, String prefix) {
+	static <T> String _toString(Iterable<T> iterable, String prefix, boolean withClass) {
 		if (iterable == null) {
 			return "NULL";
 		}
 		StringBuilder res = new StringBuilder();
 		//res.append(prefix);
-		res.append("[");
+		if (withClass) {
+			res.append(String.format("(%s) [", iterable.getClass().getCanonicalName()));
+		} else {
+			res.append("[");
+		}
 		int i = 0;
 		for (T t : iterable) {
 			res.append("\n");
 			res.append(prefix);
 			res.append(PREFIX);
-			res.append(_toString(t, prefix + PREFIX));
+			res.append(_toString(t, prefix + PREFIX, withClass));
 			i++;
 		}
 		if (i > 0) {
@@ -174,24 +249,30 @@ public interface TestCase {
 		return res.toString();
 	}
 
-	static <T> String _toString(T t) {
-		return _toString(t, "");
+	static <T> String _toString(T t, boolean withClass) {
+		return _toString(t, "", withClass);
 	}
 
 	@SuppressWarnings("unchecked")
-	static <T> String _toString(T t, String prefix) {
+	static <T> String _toString(T t, String prefix, boolean withClass) {
 		if (t == null) {
 			return prefix + "NULL";
 		}
 		if (t instanceof Map map) {
-			return _toString(map, prefix);
+			return _toString(map, prefix, withClass);
 		}
 		if (t instanceof MapDictionary map) {
-			return _toString(map.toMap(), prefix);
+			return _toString(map.toMap(), prefix, withClass);
  		}
 		if (t instanceof Iterable iterable) {
-			return _toString(iterable, prefix);
+			return _toString(iterable, prefix, withClass);
 		}
-		return t.toString(); // TODO maybe some flag to use this?  + " (" + t.getClass() + ")";
+		if (t.getClass().isArray()) {
+			return _toString(Arrays.asList((T[])t), prefix, withClass);
+		}
+		if (withClass) {
+			return t.toString() + " (" + t.getClass().getCanonicalName() + ")";
+		}
+		return t.toString();
 	}
 }
