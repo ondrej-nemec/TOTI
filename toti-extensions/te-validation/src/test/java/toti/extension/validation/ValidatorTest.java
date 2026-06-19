8ce7d95c416fc3467d1143b9419299001ba5f010
Ondřej Němec <nikdynikde@gmail.com>
2026-06-19 19:49:21 +0000

    validator added full test

--- toti-extensions/te-validation/src/test/java/toti/extension/validation/ValidatorTest.java
+++ toti-extensions/te-validation/src/test/java/toti/extension/validation/ValidatorTest.java
@@ -1,10 +1,14 @@
 package toti.extension.validation;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import static org.mockito.Mockito.mock;
@@ -17,9 +21,363 @@ import toti.extension.validation.results.ValidationItem;
 import toti.extension.validation.rules.CustomValidationRule;
 import toti.lib.common.structures.ListInit;
 import toti.lib.common.structures.MapInit;
+import toti.lib.common.tests.TestCase;
 import toti.lib.tcpip.structures.RequestParameters;
+import toti.lib.tcpip.structures.UploadedFile;
 
 public class ValidatorTest {
+
+	@Test
+	public void fullTest() {
+		RequestParameters dataToValidate = new RequestParameters();
+		RequestParameters validatedData = new RequestParameters();
+		Map<String, Object> expectedErrors = new HashMap<>();
+
+		Validator validator = Validator.create(true);
+
+		for (int i = 0; i < 3; i++) {
+			var index = i;
+			validator.addRule(r->r.booleanRules("bool_" + index, true));
+		}
+		{
+			dataToValidate.put("bool_0", null);
+			expectedErrors.put("bool_0", new ListInit<>()
+				.add("toti.validation.item-required {parameter=bool_0}")
+			.toSet());
+			validatedData.put("bool_0", null);
+		}
+		{
+			dataToValidate.put("bool_1", "");
+			validatedData.put("bool_1", false);
+		}
+		{
+			dataToValidate.put("bool_2", "0");
+			validatedData.put("bool_2", false);
+		}
+
+		for (int i = 0; i < 3; i++) {
+			int index = i;
+			validator.addRule(
+				r->r.numberRules("number_" + index, true, Integer.class)
+				.setAllowedValues(Arrays.asList(111, 222))
+				.setMaxLength(2).setMinLength(8)
+				.setMaxValue(5).setMinValue(999)
+				.setRegex("[a-c]")
+			);
+		}
+		validator.addRule(r->r.numberRules("number_3", false, Double.class));
+		validator.addRule(r->r.numberRules("validNumber", true, Integer.class));
+		{
+			dataToValidate.put("number_0", "abcd");
+			expectedErrors.put("number_0", new ListInit<>()
+				.add("toti.validation.value-type-must-be {class=java.lang.Integer}")
+			.toSet());
+			validatedData.put("number_0", "abcd");
+		}
+		{
+			dataToValidate.put("number_1", "12");
+			expectedErrors.put("number_1", new ListInit<>()
+				.add("toti.validation.value-must-be-one-of {values=[111, 222]}")
+			.toSet());
+			validatedData.put("number_1", 12);
+		}
+		{
+			dataToValidate.put("number_2", "111");
+			expectedErrors.put("number_2", new ListInit<>()
+				.add("toti.validation.value-must-be-less-or-equals {maxValue=5}")
+				.add("toti.validation.length-must-be-at-least {minLength=8}")
+				.add("toti.validation.text-not-match-pattern {regex=[a-c]}")
+				.add("toti.validation.value-must-be-equals-or-higher {minValue=999}")
+				.add("toti.validation.length-must-be-max {maxLength=2}")
+			.toSet());
+			validatedData.put("number_2", 111);
+		}
+		{
+			dataToValidate.put("number_3", "abc");
+			expectedErrors.put("number_3", new ListInit<>()
+				.add("toti.validation.value-type-must-be {class=java.lang.Double}")
+			.toSet());
+			validatedData.put("number_3", "abc");
+		}
+		{
+			dataToValidate.put("validNumber", "12.3");
+			validatedData.put("validNumber", 12);
+		}
+
+		for (int i = 0; i < 3; i++) {
+			int index = i;
+			validator.addRule(
+				r->r.objectRules("string_" + index, true)
+				.setAllowedValues(Arrays.asList("aa", "bb", "dddd"))
+				.setType(String.class)
+				.setMaxLength(2).setMinLength(8)
+				.setRegex("[a-c]")
+			);
+		}
+		validator.addRule(r->r.objectRules("validString", true));
+		{
+			dataToValidate.put("string_0", "ccccc");
+			expectedErrors.put("string_0", new ListInit<>()
+				.add("toti.validation.value-must-be-one-of {values=[aa, bb, dddd]}")
+			.toSet());
+			validatedData.put("string_0", "ccccc");
+		}
+		{
+			dataToValidate.put("string_1", "dddd");
+			expectedErrors.put("string_1", new ListInit<>()
+				.add("toti.validation.length-must-be-at-least {minLength=8}")
+				.add("toti.validation.text-not-match-pattern {regex=[a-c]}")
+				.add("toti.validation.length-must-be-max {maxLength=2}")
+			.toSet());
+			validatedData.put("string_1", "dddd");
+		}
+		{
+			expectedErrors.put("string_2", new ListInit<>()
+				.add("toti.validation.item-required {parameter=string_2}")
+			.toSet());
+			validatedData.put("string_2", null);
+		}
+		{
+			dataToValidate.put("validString", "something");
+			validatedData.put("validString", "something");
+		}
+
+		validator.addRule(
+			r->r.fileRules("file", true)
+			.setAllowedFileTypes(Arrays.asList("txt", "md"))
+			.setFileMaxSize(10).setFileMinSize(20)
+		);
+		validator.addRule(r->r.fileRules("validFile", true));
+
+		{
+			dataToValidate.put("file", new UploadedFile("fileName", "type", "bom", new byte[15]));
+			expectedErrors.put("file", new ListInit<>()
+				.add("toti.validation.file-size-can-be-max {fileMaxSize=10}")
+				.add("toti.validation.file-type-is-not-allowed {allowedFileTypes=[txt, md]}")
+				.add("toti.validation.file-size-must-be-at-least {fileMinSize=20}")
+			.toSet());
+			validatedData.put("file", new UploadedFile("fileName", "type", "bom", new byte[15]));
+		}
+		{
+			dataToValidate.put("validFile", new UploadedFile("fileName", "type", "bom", new byte[15]));
+			validatedData.put("validFile", new UploadedFile("fileName", "type", "bom", new byte[15]));
+		}
+
+		for (int i = 0; i < 2; i++) {
+			int index = i;
+			validator.addRule(
+				r->r.listRules("list_" + index, true, Validator.create(
+					s->s.numberRules(Double.class)
+				)).setMaxLength(2).setMinLength(5)
+			);
+		}
+		validator.addRule(r->r.listRules("validList", true, Validator.create(s->s.numberRules(Double.class))));
+
+		{
+			dataToValidate.put("list_0", "aaaaa");
+			expectedErrors.put("list_0", new ListInit<>()
+				.add("toti.validation.parameter-cannot-be-converted")
+			.toSet());
+			validatedData.put("list_0", "aaaaa");
+		}
+		{
+			dataToValidate.put("list_1", Arrays.asList("a", "1", 2, "d"));
+			expectedErrors.put("list_1", new ListInit<>()
+				.add("toti.validation.length-must-be-at-least {minLength=5}")
+				.add(
+					MapInit.create()
+					.append("0", new ListInit<>()
+						.add("toti.validation.value-type-must-be {class=java.lang.Double}")
+					.toSet())
+					.append("3", new ListInit<>()
+						.add("toti.validation.value-type-must-be {class=java.lang.Double}")
+					.toSet())
+					.toMap()
+				)
+				.add("toti.validation.length-must-be-max {maxLength=2}")
+			.toSet());
+			validatedData.put("list_1", Arrays.asList("a", 1.0, 2.0, "d"));
+		}
+		{
+			dataToValidate.put("validList", Arrays.asList("12.3", "45"));
+			validatedData.put("validList", Arrays.asList(12.3, 45.0));
+		}
+		
+		for (int i = 0; i < 2; i++) {
+			int index = i;
+			validator.addRule(
+				r->r.mapRules("map_" + index, true, Validator.create(true)
+					.addRule(s->s.numberRules("a", true, Integer.class))
+					.addRule(s->s.numberRules("b", false, Integer.class).setMaxValue(10))
+				)
+				.setMaxLength(2)
+				.setMinLength(10)
+			);
+		}
+		validator.addRule(r->r.mapRules("validMap", true, Validator.create(false)
+			.addRule(s->s.numberRules("a", true, Integer.class))
+			.addRule(s->s.numberRules("b", true, Double.class))
+		));
+		{
+			dataToValidate.put("map_0", "aaaaa");
+			expectedErrors.put("map_0", new ListInit<>()
+				.add("toti.validation.parameter-cannot-be-converted")
+			.toSet());
+			validatedData.put("map_0", "aaaaa");
+		}
+		{
+			dataToValidate.put("map_1", new RequestParameters()
+				.put("a", 12.4)
+				.put("b", 11)
+				.put("c", 123)
+			);
+			expectedErrors.put("map_1", new ListInit<>()
+				.add(
+					MapInit.create()
+					.append("", new ListInit<>()
+						.add("toti.validation.not-expected-parameters {parameters=[map_1[c]]}")
+					.toSet())
+					.append("b", new ListInit<>()
+						.add("toti.validation.value-must-be-less-or-equals {maxValue=10}")
+					.toSet())
+					.toMap()
+				)
+				.add("toti.validation.length-must-be-max {maxLength=2}")
+				.add("toti.validation.length-must-be-at-least {minLength=10}")
+			.toSet());
+			validatedData.put("map_1", new RequestParameters().put("a", 12).put("b", 11).put("c", 123));
+		}
+		{
+			dataToValidate.put("validMap", new RequestParameters().put("a", "11.1").put("b", "22"));
+			validatedData.put("validMap", new RequestParameters().put("a", 11).put("b", 22.0));
+		}
+
+		validator.addRule(
+			r->r.numberRules("custom_0", true, Integer.class)
+			.setCustomValidation(item->{
+				item.addError("Error single");
+				item.addError("Raw " + item.getRawValue());
+				item.addError("Parsed " + item.getParsedValue());
+				item.setValue("Something");
+			})
+		);
+		{
+			dataToValidate.put("custom_0", 12.3);
+			expectedErrors.put("custom_0", new ListInit<>()
+				.add("Parsed 12")
+				.add("Raw 12.3")
+				.add("Error single")
+			.toSet());
+			validatedData.put("custom_0", "Something");
+		}
+
+		validator.addRule(
+			r->r.mapRules("custom_1", true, Validator.create(false)
+				.addRule(s->s.numberRules("number", true, Integer.class))
+				.setCustomValidation(item->{
+					item.addError("Error map");
+					item.addError("Names " + item.getItemsNames());
+					item.addError("Parsed " + item.getValue("number"));
+					item.setItem("boolean", true);
+					item.removeItem("a");
+				})
+			)
+		);
+		{
+			dataToValidate.put("custom_1", MapInit.create().append("number", 12.3).append("a", "A").append("x", "X").toMap());
+			expectedErrors.put("custom_1", new ListInit<>()
+				.add(
+					MapInit.create()
+					.append("", new ListInit<>()
+						.add("Parsed 12")
+						.add("Names [number, a, x]")
+						.add("Error map")
+					.toSet())
+					.toMap()
+				)
+			.toSet());
+			
+			validatedData.put(
+				"custom_1",
+				new RequestParameters().put("number", 12).put("x", "X").put("boolean", true)
+			);
+		}
+
+		validator.addRule(
+			r->r.mapRules("upper", true, Validator.create(true)
+				.addRule(
+					s->s.listRules("subList1", true, Validator.create(t->t.numberRules(Integer.class)))
+				)
+				.addRule(
+					s->s.listRules("subList2", true, Validator.create(
+						t->t.numberRules(Integer.class)
+						.setMaxValue(5).setMinValue(20)
+					))
+					.setMaxLength(2).setMinLength(10)
+				)
+				.addRule(
+					s->s.mapRules("subMap", true, Validator.create(true)
+						.addRule(t->t.numberRules("a", false, Double.class))
+						.addRule(t->t.numberRules("b", true, Double.class))
+					)
+				)
+			)
+		);
+		dataToValidate.put(
+			"upper",
+			new RequestParameters()
+			.put("subList1", Arrays.asList())
+			.put("subList2", Arrays.asList(12, "12.3", "false", "x"))
+			.put("subMap", new RequestParameters().put("a", "x").put("c", "C"))
+		);
+		expectedErrors.put("upper", new ListInit<>()
+			.add(MapInit.create()
+			.append("subList2", new ListInit<>()
+				.add(MapInit.create()
+				.append("0", new ListInit<>()
+					.add("toti.validation.value-must-be-less-or-equals {maxValue=5}")
+					.add("toti.validation.value-must-be-equals-or-higher {minValue=20}")
+				.toSet())
+				.append("1", new ListInit<>()
+					.add("toti.validation.value-must-be-less-or-equals {maxValue=5}")
+					.add("toti.validation.value-must-be-equals-or-higher {minValue=20}")
+				.toSet())
+				.append("2", new ListInit<>()
+					.add("toti.validation.value-type-must-be {class=java.lang.Integer}")
+				.toSet())
+				.append("3", new ListInit<>()
+					.add("toti.validation.value-type-must-be {class=java.lang.Integer}")
+				.toSet())
+				.toMap())
+				.add("toti.validation.length-must-be-max {maxLength=2}")
+				.add("toti.validation.length-must-be-at-least {minLength=10}")
+			.toSet())
+			.append("subMap", new ListInit<>()
+				.add(MapInit.create()
+				.append("a", new ListInit<>()
+					.add("toti.validation.value-type-must-be {class=java.lang.Double}")
+				.toSet())
+				.append("b", new ListInit<>()
+					.add("toti.validation.item-required {parameter=b}")
+				.toSet())
+				.append("", new ListInit<>()
+					.add("toti.validation.not-expected-parameters {parameters=[upper[subMap][c]]}")
+				.toSet())
+				.toMap())
+			.toSet())
+			.toMap())
+		.toSet());
+		validatedData.put("upper", new RequestParameters()
+			.put("subList1", Arrays.asList())
+			.put("subList2", Arrays.asList(12, 12, "false", "x"))
+			.put("subMap", new RequestParameters().put("a", "x").put("b", null).put("c", "C"))
+		);
+
+		var result = validator.validate(dataToValidate);
+		assertFalse(result.isValid());
+		TestCase.assertEquals(expectedErrors, result.getErrors());
+		TestCase.assertEquals(validatedData, result.getValues());
+	}
 
 	@ParameterizedTest
 	@MethodSource
@@ -60,8 +418,8 @@ public class ValidatorTest {
 				.addRule(r->r.objectRules("key1", true)),
 				new ValidationCollection(
 					MapInit.create().toMap(),
-					new ListInit<>().add("toti.validation.not-expected-parameters").toSet(),
-					new RequestParameters().put("key1", "value1")
+					new ListInit<>().add("toti.validation.not-expected-parameters {parameters=[key2]}").toSet(),
+					new RequestParameters().put("key1", "value1").put("key2", "value2")
 				)
 			},
 			new Object[] {
@@ -92,8 +450,8 @@ public class ValidatorTest {
 				}),
 				new ValidationCollection(
 					MapInit.create().toMap(),
-					new ListInit<>().add("toti.validation.not-expected-parameters").toSet(),
-					new RequestParameters()
+					new ListInit<>().add("toti.validation.not-expected-parameters {parameters=[key1, key2]}").toSet(),
+					new RequestParameters().put("key1", "value1").put("key2", "value2")
 				)
 			},
 			new Object[] {
